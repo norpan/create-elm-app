bdfa47fa3a7357f076caf82ca991a5d80bf01e2c
norpan <norpan@users.noreply.github.com>
2018-02-20 09:24:01 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-
-USERNAME=norp
-IMAGE=create-elm-app
-VERSION=1.10.2
-ELM_VERSION=0.18
-
-docker build --build-arg VERSION=$VERSION -t $USERNAME/$IMAGE:latest -t $USERNAME/$IMAGE:$VERSION-elm-$ELM_VERSION .
